c0becd1275761f42096d2d745871c3b332cdefde
YunHyoJin <xilm1004@naver.com>
2017-10-11 10:26:51 +0900

    .

--- java-basic/src/main/java/bitcamp/java100/Test15_10.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_10.java
@@ -6,7 +6,17 @@ public class Test15_10 {
         int v1 = 0b0101_1100;
         int v2 = 0b0110_0110;
         
+        // & 논리곱 and
         int result = v1 & v2;
+        System.out.println(java.lang.Integer.toBinaryString(result));
+        // | 논리합 or
+        result = v1 | v2;
+        System.out.println(java.lang.Integer.toBinaryString(result));
+        // ^ 배타적 논리곱
+        result = v1 ^ v2;
+        System.out.println(java.lang.Integer.toBinaryString(result));
+        // ~ 부정 not
+        result = ~v1;
         System.out.println(java.lang.Integer.toBinaryString(result));
         
         
---
+++ java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
@@ -0,0 +1,46 @@
+package bitcamp.java100;
+
+public class Test15_11_1 {
+
+    public static void main(String[] args) {
+        class Priviliege {
+            boolean isDirectory;
+            boolean ownerRead;
+            boolean ownerWrite;
+            boolean ownerExe;
+            boolean groupRead;
+            boolean groupWrite;
+            boolean groupExe;
+            boolean otherRead;
+            boolean otherWrite;
+            boolean otherExe;
+        }
+        
+        Priviliege p1 = new Priviliege();
+        
+        p1.isDirectory = false;
+        p1.ownerRead = true;
+        p1.ownerWrite = true;
+        p1.ownerExe = true;
+        p1.groupRead = true;
+        p1.groupWrite = false;
+        p1.groupExe = true;
+        p1.otherRead = true;
+        p1.otherWrite = false;
+        p1.otherExe = true;
+        
+        System.out.print(p1.isDirectory ? "d" : "-");
+        System.out.print(p1.ownerRead ? "r" : "-");
+        System.out.print(p1.ownerWrite ? "w" : "-");
+        System.out.print(p1.ownerExe ? "x" : "-");
+        System.out.print(p1.groupRead ? "r" : "-");
+        System.out.print(p1.groupWrite ? "w" : "-");
+        System.out.print(p1.groupExe ? "x" : "-");
+        System.out.print(p1.otherRead ? "r" : "-");
+        System.out.print(p1.otherWrite ? "w" : "-");
+        System.out.print(p1.otherExe ? "x" : "-");
+        System.out.println();
+        
+
+    }
+}
