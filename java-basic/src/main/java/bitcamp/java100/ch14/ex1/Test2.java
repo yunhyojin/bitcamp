305465af8220d6c29296db8deeaf62431c082c1d
yunhyojin <xilm1004@naver.com>
2017-11-09 17:34:00 +0900

    2017-11-09

--- java-basic/src/main/java/bitcamp/java100/ch14/ex1/Test2.java
+++ java-basic/src/main/java/bitcamp/java100/ch14/ex1/Test2.java
@@ -11,8 +11,16 @@ public class Test2 {
         System.out.printf("파일생성=%b\n",file.createNewFile());
         System.out.printf("파일삭제=%b\n",file.delete());
         
-        File file2 = new File("dir");
-        file2
+        File file2 = new File("dir1");
+        System.out.printf("디렉토리 생성=%b\n",file2.mkdir());
+        System.out.printf("디렉토리 삭제=%b\n",file2.delete());
+        
+        File file3 = new File("aaa/bbb/ccc/test.txt");
+        System.out.printf("디렉토리2 생성=%b\n", file3.mkdir());
+        System.out.printf("디렉토리2 삭제=%b\n",file3.delete());
+       
+        System.out.printf("디렉토리2 생성=%b\n", file3.mkdirs());
+        System.out.printf("디렉토리2 삭제=%b\n",file3.delete());
     }
 
 }
